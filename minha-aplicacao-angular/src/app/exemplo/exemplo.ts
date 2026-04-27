1f640efe120546aa73b7d578296be92c1fafd9dd
Calebe Carvalho <calebecvsts@gmail.com>
2026-04-27 20:03:22 -0300

    aula 1 angular fundamentos

---
+++ minha-aplicacao-angular/src/app/exemplo/exemplo.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo',
+  imports: [],
+  templateUrl: './exemplo.html',
+  styleUrl: './exemplo.css',
+})
+export class Exemplo {
+  nome = "Fabio";
+
+  mudarNome() {
+    this.nome = "Angular Dev";
+  }
+}
