5fe9452b3f05f2230b304f4919716b2618959d64
Calebe Carvalho <calebecvsts@gmail.com>
2026-04-28 17:54:52 -0300

    aula 4 navegacao

--- minha-aplicacao-angular/src/app/app.routes.ts
+++ minha-aplicacao-angular/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+// import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'produtos',
+        pathMatch: 'full',
+    },
+    {
+        path: 'produtos',
+        loadComponent: () =>
+            import('./pages/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
+    },
+    {
+        path: 'produto/:id',
+        loadComponent: () =>
+            import('./pages/detalhe-produto/detalhe-produto').then((m) => m.DetalheProduto),
+    },
+    {
+        path: 'painel-adm',
+        loadComponent: () => import('./painel-adm/painel-adm').then((m) => m.PainelAdm),
+    },
+    {
+        path: '**',
+        redirectTo: 'produtos',
+    }
+];
--- minha-aplicacao-angular/src/app/app.ts
+++ minha-aplicacao-angular/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { Aula3 } from './aula3/aula3';
-import { PainelAdm } from './painel-adm/painel-adm';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Aula3, PainelAdm],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
