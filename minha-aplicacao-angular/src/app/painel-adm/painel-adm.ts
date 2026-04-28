129b4c32de002c4f7996dff9689a1eea161788e9
Calebe Carvalho <calebecvsts@gmail.com>
2026-04-28 15:47:50 -0300

    aula 2 diretivas

--- minha-aplicacao-angular/src/app/painel-adm/painel-adm.ts
+++ minha-aplicacao-angular/src/app/painel-adm/painel-adm.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+interface Usuario {
+  id: number;
+  nome: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-painel-adm',
   standalone: true,
@@ -8,4 +14,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './painel-adm.html',
   styleUrl: './painel-adm.css'
 })
-export class PainelAdm { }
+export class PainelAdm {
+
+  usuarios: Usuario[] = [
+    { id: 1, nome: "Marcos", email: "marcos@email.com" },
+    { id: 2, nome: "Marcia", email: "marcia@email.com" },
+    { id: 1, nome: "Flavio", email: "flavio@email.com" },
+  ]
+}
